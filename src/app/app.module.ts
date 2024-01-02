81977fe0a06dd755f2f5159a758e73bd2221c944
BlokusPokus <121772251+BlokusPokus@users.noreply.github.com>
2024-01-02 14:05:05 -0500

    Service Bancaire Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { CustomersComponent } from './customers/customers.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { CustomerAccountsComponent } from './customer-accounts/customer-accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AccountsComponent,
+    CustomersComponent,
+    NewCustomerComponent,
+    CustomerAccountsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new-customer/new-customer.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {Form, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Customer} from "../model/customer.model";
+import {CustomerService} from "../services/customer.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-new-customer',
+  templateUrl: './new-customer.component.html',
+  styleUrl: './new-customer.component.css'
+})
+export class NewCustomerComponent {
+  newCustomerFromGroup!: FormGroup;
+  constructor(private fb : FormBuilder, private customerService:CustomerService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.newCustomerFromGroup=this.fb.group({
+      name: this.fb.control(null, [Validators.required, Validators.minLength(4)]),
+      email: this.fb.control(null, [Validators.required,Validators.email]),
+    });
+  }
+
+  handleSaveCustomer() {
+    let customer:Customer=this.newCustomerFromGroup.value;
+    this.customerService.saveCustomer(customer).subscribe({
+      next:data=>{
+        alert("Customer has been created successfully")
+        //this.newCustomerFromGroup.reset();
+        this.router.navigateByUrl("/customers")
+      },
+      error: err=>{
+        console.log(err);
+      }
+    })
+  }
+}
